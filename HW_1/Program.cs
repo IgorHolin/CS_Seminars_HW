1fb1165f4a51abdc7fc18fdf99e11b59022af7c3
Igor <igor.paluhovicz@gmail.com>
2022-06-25 15:12:04 +0200

    Ex_6 done

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,16 +1,11 @@
-﻿Console.Write("Enter first number: ");
-int firstN = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter second number: ");
-int secondN = Convert.ToInt32(Console.ReadLine());
-    if (firstN > secondN)
-    {
-        Console.WriteLine("First number is bigger: ");
-        Console.Write("max = ");
-        Console.WriteLine(firstN);
-    }
-    else
-    {
-        Console.WriteLine("Second number is bigger");
-        Console.Write("max = ");
-        Console.WriteLine(secondN); 
-    }
+﻿Console.Write("Enter the number, please: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётоне");
+}
+
