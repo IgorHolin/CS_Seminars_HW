40164dd3c9e3297c4b40ff4ad78e81fd019b9acf
Igor <igor.paluhovicz@gmail.com>
2022-06-25 14:40:08 +0200

    X

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.Write("Enter first number: ");
+int firstN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondN = Convert.ToInt32(Console.ReadLine());
+    if (firstN > secondN)
+    {
+        Console.WriteLine("First number is bigger: ");
+        Console.Write("max = ");
+        Console.WriteLine(firstN);
+    }
+    else
+    {
+        Console.WriteLine("Second number is bigger");
+        Console.Write("max = ");
+        Console.WriteLine(secondN); 
+    }
