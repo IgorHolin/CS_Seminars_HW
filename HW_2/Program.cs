b449bac1f85c6d12b85fee44ea26d1f3500a6186
Igor <igor.paluhovicz@gmail.com>
2022-06-26 11:59:55 +0200

    Ex_10

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу,
+// которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+//Знаю что можно без функции, но балуюсь, чтобы закрепить материал :)
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Your number is: {number}");
+
+int SecondNumber(int number)
+{
+    int answer = (number / 10) % 10;
+    return answer;
+}
+
+Console.WriteLine($"The middle number of {number} is {SecondNumber(number)}");
