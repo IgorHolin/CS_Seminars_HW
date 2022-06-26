d49ef3c033d218d5f95e7d8b352229075a278825
Igor <igor.paluhovicz@gmail.com>
2022-06-26 12:14:48 +0200

    Ex_10_done

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -1,11 +1,11 @@
-﻿// Задача 10: Напишите программу,
-// которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
 
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
 
-//Знаю что можно без функции, но балуюсь, чтобы закрепить материал :)
+//Балуюсь с функциями :)
 
 
 int number = new Random().Next(100, 1000);
@@ -16,5 +16,4 @@ int SecondNumber(int number)
     int answer = (number / 10) % 10;
     return answer;
 }
-
-Console.WriteLine($"The middle number of {number} is {SecondNumber(number)}");
+Console.WriteLine($"The second number in {number} is {SecondNumber(number)}");
