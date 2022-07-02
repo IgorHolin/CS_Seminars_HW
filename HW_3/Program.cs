4ec8cd26142cb4518f7db04ae68130830e78e033
Igor <igor.paluhovicz@gmail.com>
2022-07-02 12:43:27 +0200

    Ex_19_done

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Enter the number please: ");
+string number = Console.ReadLine();
+
+bool PolyOrNot(string num)
+{
+    return (num[0] == num[4] && num[1] == num[3]);
+}
+
+Console.WriteLine(PolyOrNot(number));
