cfdb0364989d075ffd72de34f73a381e417e1d7f
Igor <igor.paluhovicz@gmail.com>
2022-07-02 13:20:32 +0200

    Ex_23_done

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -1,24 +1,10 @@
-﻿// Напишите программу, которая принимает на вход координаты двух точек 
-// и находит расстояние между ними в 3D пространстве.
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
 
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
 
-// d = sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2
+Console.Write("Enter N digit please: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter x1: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter y1: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter z1: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter x2: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter y2: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter z2: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
-
-double d = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + (Math.Pow(z2 - z1, 2))));
-
-Console.WriteLine($"Расстояние между точками приблизительно равно: {d}");
+for (int i = 1; i <= N; i++) { Console.WriteLine($"The cube of {i} is following: {Math.Pow(i, 3)}"); }
