1445816f49a8be45f9bd04a2ad7fb8dbe1f4c970
Igor <igor.paluhovicz@gmail.com>
2022-07-03 13:45:58 +0200

    Ex_25_done

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -1,23 +1,20 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число 
-// и выдаёт сумму цифр в числе.
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
 
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
 
-// Функции функции, практикуюсь :)
 
-int sumOf(int num)
+
+
+double PowerOf(int num, int power)
 {
-    int result = 0;
-    while (num / 10 != 0)
-    {
-        result = result + (num % 10);
-        num = num / 10;
-    }
-    return result + num%10;
+    double result = Math.Pow(num, power);
+    return result;
 }
 
-Console.Write("Enter the number please: ");
+Console.Write("Enter the base please: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"The sum of digits in {number} is: {sumOf(number)}");
+Console.Write("Enter the power please: ");
+int pow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Number {number} in {pow} power gives us: {PowerOf(number,pow)}");
