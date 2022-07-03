93ba7e4e76b78cc2d5712c7a9e3ddaf6e329580d
Igor <igor.paluhovicz@gmail.com>
2022-07-03 12:37:58 +0200

    HW_done

---
+++ HW_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+//Попробовал заполняшки с рандомом через void-функции
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 1000);
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine($"[{String.Join(",", arr)}]");
+}
+  
+
+int[] newArr = new int[8];
+FillArray(newArr);
+PrintArray(newArr);
