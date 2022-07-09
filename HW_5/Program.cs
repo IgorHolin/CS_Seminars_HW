20bcde8b46e2f8bf168b7b975e8d2b794f678b1d
Igor <igor.paluhovicz@gmail.com>
2022-07-09 13:52:06 +0200

    Ex_38_done

--- HW_5/Program.cs
+++ HW_5/Program.cs
@@ -1,22 +1,19 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
-// Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
+// [3 7 22 2 78] -> 76
 
 Console.Write("Enter the size of array please: ");
 int arraySize = Convert.ToInt32(Console.ReadLine());
-int[] newArray = new int[arraySize];
-int unevenSum = 0;
+double[] array = new double[arraySize];
 
 for (int i = 0; i < arraySize; i++)
 {
-    newArray[i] = new Random().Next(-10, 11);
-    if (i % 2 != 0) 
-    {
-        Console.WriteLine($"Element {newArray[i]} has uneven index {i}"); // можно без райтлайна и короче, но с выводом в консоли
-        unevenSum += newArray[i];                                         // получается удобно искать и считать :)
-    }
+    array[i] = new Random().NextDouble(); //чтоб не морочаться сделал просто рандомДабл заполняшку
 }
-Console.WriteLine($"Your array is: [{String.Join(",", newArray)}]");
-Console.WriteLine($"The sum of elements with uneven index is: {unevenSum}");
+Console.WriteLine($"Array: [{String.Join("; ", array)}]");
+double maxDigit = array.Max();
+Console.WriteLine($"Maximal digit in array is: {maxDigit}");
+double minDigit = array.Min();
+Console.WriteLine($"Minimal digit in array: {minDigit}");
+Console.Write($"The difference between maximal and minimal digits is: {maxDigit - minDigit}");
+
