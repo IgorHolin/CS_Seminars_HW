6c43b4879b66599dc00d65539c7550a80e236002
Igor <igor.paluhovicz@gmail.com>
2022-07-16 13:39:56 +0200

    Ex_50_done

--- HW_7/Program.cs
+++ HW_7/Program.cs
@@ -1,52 +1,53 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n
-// заполненный случайными вещественными числами.
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+// Например, задан массив:
 
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
 
+// 17 -> такого числа в массиве нет
+// Решал по нахождению числа под введённым индексом и функциями (давно не делал)
 
-// 1 Вариант 
-
-Console.Write("Enter the number of rows please: ");
+Console.Write("Set the width of matrix: ");
 int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the number of columns please: ");
+Console.Write("Set the heigth of matrix: ");
 int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
 
-double[,] matrix = new double[rows, columns];
-
-for (int i = 0; i < rows; i++)
-{
-    for (int j = 0; j < columns; j++)
-    {
-        matrix[i, j] = new Random().NextDouble();
-        Console.Write($"{Math.Round(matrix[i, j]*100, 1)}    ");
-    }
-    Console.WriteLine();
-}
-
-
-// 2 Вариант через метод
-
-Console.Write("Enter the number of rows please: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter the number of columns please: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-
-double[,] matrix = new double[rows, columns];
-
-void FillAndPrintMatrix(double[,] matr)
+void FillAndPrintMatrix(int[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i, j] = new Random().NextDouble();
-            Console.Write($"{Math.Round(matr[i, j] * 100, 1)}    ");
+            matr[i, j] = new Random().Next(1, 9);
+            Console.Write($"{matr[i, j]}    ");
         }
         Console.WriteLine();
     }
 }
-FillAndPrintMatrix(matrix);
+
+void InOrNot(int[,] mat, int row, int column)
+
+{
+    if (row < mat.GetLength(0) && column < mat.GetLength(1))
+    {
+        for (int i = 0; i < mat.GetLength(0); i++)
+        {
+            for (int j = 0; j < mat.GetLength(1); j++)
+            {
+                if (i == row && j == column) Console.WriteLine($"The digit under this index is: {mat[i, j]}");
+            }
+        }
+    }
+    else Console.WriteLine("Number doesnt exist");
+}
+
+    FillAndPrintMatrix(matrix);
+    Console.Write("Enter the first index: ");
+    int firstIndex = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the second index: ");
+    int secondIndex = Convert.ToInt32(Console.ReadLine());
+    InOrNot(matrix, firstIndex, secondIndex);
