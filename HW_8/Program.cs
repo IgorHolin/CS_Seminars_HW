e8e251342b2949f8486af2642c5713eb469fdeff
Igor <igor.paluhovicz@gmail.com>
2022-07-20 14:03:06 +0200

    init

--- HW_8/Program.cs
+++ HW_8/Program.cs
@@ -1,8 +1,38 @@
-﻿// Задача 60. Сформируйте массив 3x3 из неповторяющихся двузначных чисел.
-// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+﻿// Задача 62. Заполните спирально массив 4 на 4.
 
-// массив размером 2 x 2 x 2
+// Например, на выходе получается вот такой массив:
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
 
-// 12(0,0,0) 22(0,0,1)
+int n = 4;
+int[,] matrix = new int[n, n];
 
-// 45(1,0,0) 53(1,0,1)
+int counter = 1;
+int begrow = 0;
+int endcol = n-1;
+int endrow = n-1;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    matrix[begrow,i] = counter;
+    counter++;
+}
+begrow++;
+
+for (int i = begrow; i < matrix.GetLength(1); i++)
+{
+    matrix[endcol,i] = counter;
+    counter++;
+}
+endcol--;
+
+for (int i = endcol; i > 0 ; i--)
+{
+    matrix[endrow,endcol] = counter;
+    counter++;
+}
+endrow--;
+
+for ()
