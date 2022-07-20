7e926799d4aaaeb1a5a4264f0473d81627474484
Igor <igor.paluhovicz@gmail.com>
2022-07-20 14:18:23 +0200

    reset

--- HW_8/Program.cs
+++ HW_8/Program.cs
@@ -6,33 +6,3 @@
 // 11 16 15 6
 // 10 9 8 7
 
-int n = 4;
-int[,] matrix = new int[n, n];
-
-int counter = 1;
-int begrow = 0;
-int endcol = n-1;
-int endrow = n-1;
-
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    matrix[begrow,i] = counter;
-    counter++;
-}
-begrow++;
-
-for (int i = begrow; i < matrix.GetLength(1); i++)
-{
-    matrix[endcol,i] = counter;
-    counter++;
-}
-endcol--;
-
-for (int i = endcol; i > 0 ; i--)
-{
-    matrix[endrow,endcol] = counter;
-    counter++;
-}
-endrow--;
-
-for ()
